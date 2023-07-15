bd42cf16b74e9d8bd34772c77e05f021ac30e0b8
rus <corponat@yandex.ru>
2023-07-15 10:25:48 +0300

    change strategy

--- src/Resources/skeleton/doctrine/Entity.tpl.php
+++ src/Resources/skeleton/doctrine/Entity.tpl.php
@@ -16,7 +16,7 @@ namespace <?= $namespace ?>;
 class <?= $class_name."\n" ?>
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue(strategy: 'IDENTITY')]
+    #[ORM\GeneratedValue(strategy: 'SEQUENCE')]
     #[ORM\Column(type: 'integer')]
     private ?int $id = null;
 
